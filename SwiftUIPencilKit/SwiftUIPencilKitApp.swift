866ea541ffb5ab1d20d6c7e24fb9a198f5e7b4b9
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-08-21 14:49:14 +0400

    Initial Commit

---
+++ SwiftUIPencilKit/SwiftUIPencilKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPencilKitApp.swift
+//  SwiftUIPencilKit
+//
+//  Created by Hakob Ghlijyan on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPencilKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
